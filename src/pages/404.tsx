736aec84b180fea0b86d2875c7f58cbda2203013
candicecz <czech.candice@gmail.com>
2023-05-23 15:29:07 -0300

    fix: dom attribute error

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { NextPage } from 'next';
 import {
   Button,
   Flex,
@@ -5,9 +7,8 @@ import {
   Image,
   Text,
 } from '@candicecz/test-design-system';
-import { NextPage } from 'next';
-import React from 'react';
 import { PageContainer, PageContent } from 'src/components/page-container';
+import NextLink from 'next/link';
 
 const NotFoundPage: NextPage = () => {
   return (
@@ -23,7 +24,7 @@ const NotFoundPage: NextPage = () => {
             src='/assets/404.png'
             alt='404: Page Not Found'
             mb={8}
-          ></Image>
+          />
           <Heading as='h1' my={4}>
             The page you’re looking for isn’t available.
           </Heading>
@@ -31,9 +32,11 @@ const NotFoundPage: NextPage = () => {
             It&apos;s possible that the page is temporarily unavailable, has
             been moved, renamed, or no longer exists.
           </Text>
-          <Button as='a' href='/' mt={4} variant='outline' h='unset'>
-            Back to Home
-          </Button>
+          <NextLink href={{ pathname: '/' }}>
+            <Button mt={4} variant='outline' h='unset'>
+              Back to Home
+            </Button>
+          </NextLink>
         </Flex>
       </PageContent>
     </PageContainer>
